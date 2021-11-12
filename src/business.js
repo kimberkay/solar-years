ccf5fb52dd91bed7543e7a97968e03e2866a4f0d
Kim Brannian <brannian@gmail.com>
2021-11-12 09:57:10 -0800

    refactor second test

--- src/business.js
+++ src/business.js
@@ -4,7 +4,12 @@ export default class SolarYears {
   constructor(earthYears, earthExpectancy) {
     this.earthYears = earthYears;
     this.earthExpectancy = earthExpectancy;
+    /*this.yearsPast = yearsPast;*/
   }
     
+  /*pastExpectancy() {
+    this.earthExpectancy - this.earthYears = this.yearsPast;
+    return yearsPast;
+  }*/
 
 }
