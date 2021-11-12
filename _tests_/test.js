44f30e93ef18ee3cae43b65029fe86280aba7ad7
Kim Brannian <brannian@gmail.com>
2021-11-12 08:53:26 -0800

    debug first test

---
+++ _tests_/test.js
@@ -0,0 +1,10 @@
+import SolarYears from './../src/js/business.js';
+
+describe('SolarYears', () => {
+
+  test('should create a SolarYears object with earthYears and earthExpectancy', () => {
+    const myYears = new SolarYears(41, 85);
+    expect(myYears.earthYears).toEqual(41);
+    expect(myYears.earthExpectancy).toEqual(85); 
+  });
+});
