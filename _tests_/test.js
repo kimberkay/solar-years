317c6e8c941e1455830ae1935965b27309d20395
Kim Brannian <brannian@gmail.com>
2021-11-28 14:35:45 -0800

    add green test

--- _tests_/test.js
+++ _tests_/test.js
@@ -1,4 +1,4 @@
-import SolarYears from './../src/business.js';
+import SolarYears from './../src/SolarYears.js';
 
 describe('SolarYears', () => {
 
@@ -12,14 +12,14 @@ describe('SolarYears', () => {
     expect(myYears.yearsOver()).toEqual(-44);
   });
  
-  test('should correctly refactor mercuryYears to work for all planets', () => {
+  test('should correctly return age in Mercury years', () => {
     const myYears = new SolarYears(41,85);
-    expect(myYears.onAnotherPlanetAge()).toEqual(171); 
+    expect(myYears.mercuryAge()).toEqual(171);
 });
 
-test('should correctly return life expectancy on another planet', () => {
+/*test('should correctly return life expectancy on Mercury', () => {
   const myYears = new SolarYears(41,85);
-  expect(myYears.expectancyOnAnotherPlanet()).toEqual(354); 
+  expect(myYears.mercuryExpectancy()).toEqual(354); 
 });
 
 test('should correctly return years left on other planets', () => {
@@ -29,7 +29,7 @@ test('should correctly return years left on other planets', () => {
 
 test('should correctly return planet years over life expectancy', () => {
   const elder = new SolarYears(92,85)
-  expect(elder.yearsOverOnOtherPlanet()).toEqual(29)
+  expect(elder.yearsOverOnOtherPlanet()).toEqual(29)*/
 });
 
 
@@ -39,4 +39,5 @@ test('should correctly return planet years over life expectancy', () => {
 
 
 
-});
+
+
---
+++ src/SolarYears.js
@@ -0,0 +1,41 @@
+
+
+export default class SolarYears {
+  constructor(earthYears, earthExpectancy, age, expectancy) {
+    this.earthYears = earthYears;
+    this.earthExpectancy = earthExpectancy;
+    this.mercury = .24;
+    this.jupiter = 11.86;
+    this.venus = .62;
+    this.mars = 1.88;
+    this.age = 0;
+    this.expectancy = 0;
+    
+  }
+    
+    yearsOver() {
+      let overExpectancy = (this.earthYears - this.earthExpectancy);
+      return overExpectancy;
+    }
+
+    mercuryAge() {
+      this.age = Math.round(((this.earthYears/0.24) * 100)/100);
+      return this.age;
+    }
+
+    /*mercuryExpectancy() {
+      this.expectancy = Math.round(this.earthExpectancy/this.mercury);
+      return this.expectancy;
+  }    
+
+    yearsLeftOnMercury() {
+      let yearsLeft = Math.round(this.mercuryExpectancy()-this.mercuryAge());
+      return yearsLeft;
+  }
+
+    yearsOverMercury() {
+      let yearsOver = Math.round(this.yearsOver()/this.mercury)
+      return yearsOver;
+  }*/
+}
+
