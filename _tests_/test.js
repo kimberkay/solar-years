214d851355362766a4d57f061102051e27bb6762
Kim Brannian <brannian@gmail.com>
2021-11-28 19:59:17 -0800

    add green tests

--- _tests_/test.js
+++ _tests_/test.js
@@ -42,16 +42,55 @@ const myYears = new SolarYears(41,85);
 expect(myYears.marsExpectancy()).toEqual(45);
 });
 
-test('should correctly return years left or over on Mercury with message for each scenario', () => {
+test('should correctly return years left or over on Mars with message for each scenario', () => {
 const myYears = new SolarYears(41,85);
 expect(myYears.marsYearsLeftOrOver()).toEqual("\"You have 23 years left. Go have more fun!\"")
 });
 
-test('should correctly return years left or over on Mercury with message for each scenario', () => {
+test('should correctly return years left or over on Mars with message for each scenario', () => {
 const olderYears = new SolarYears(87,85);
 expect(olderYears.marsYearsLeftOrOver()).toEqual(`"You are 1 years over your expected years. Go have more fun!`)
 });
 
+test('should correctly return age in Jupiter years', () => {
+  const myYears = new SolarYears(41,85);
+  expect(myYears.jupiterAge()).toEqual(3);
+});
+
+test('should correctly return expectancy in Jupiter years', () => {
+  const myYears = new SolarYears(41,85);
+  expect(myYears.jupiterExpectancy()).toEqual(7);
+  });
+  
+  test('should correctly return years left or over on Mars with message for each scenario', () => {
+  const myYears = new SolarYears(41,85);
+  expect(myYears.jupiterYearsLeftOrOver()).toEqual("\"You have 4 years left. Go have more fun!\"")
+  });
+  
+  test('should correctly return years left or over on Mars with message for each scenario', () => {
+  const olderYears = new SolarYears(87,85);
+  expect(olderYears.jupiterYearsLeftOrOver()).toEqual(`"You are 0 years over your expected years. Go have more fun!`)
+  });
+
+  test('should correctly return age in Venus years', () => {
+    const myYears = new SolarYears(41,85);
+    expect(myYears.venusAge()).toEqual(66);
+  });
+  
+  test('should correctly return expectancy in Venus years', () => {
+    const myYears = new SolarYears(41,85);
+    expect(myYears.venusExpectancy()).toEqual(137);
+    });
+    
+    test('should correctly return years left or over on Venus with message for each scenario', () => {
+    const myYears = new SolarYears(41,85);
+    expect(myYears.venusYearsLeftOrOver()).toEqual("\"You have 71 years left. Go have more fun!\"")
+    });
+    
+    test('should correctly return years left or over on Venus with message for each scenario', () => {
+    const olderYears = new SolarYears(87,85);
+    expect(olderYears.venusYearsLeftOrOver()).toEqual(`"You are 3 years over your expected years. Go have more fun!`)
+    });
 });
 
 
--- src/SolarYears.js
+++ src/SolarYears.js
@@ -4,9 +4,9 @@ export default class SolarYears {
   constructor(earthYears, earthExpectancy, age, expectancy) {
     this.earthYears = earthYears;
     this.earthExpectancy = earthExpectancy;
-    this.mercury = .24;
+    this.mercury = 0.24;
     this.jupiter = 11.86;
-    this.venus = .62;
+    this.venus = 0.62;
     this.mars = 1.88;
     this.age = 0;
     this.expectancy = 0;
@@ -26,7 +26,7 @@ export default class SolarYears {
     mercuryExpectancy() {
       this.expectancy = Math.round(((this.earthExpectancy/this.mercury)*100)/100);
       return this.expectancy;
-  }    
+    }    
 
     mercuryYearsLeftOrOver() {
       let mercuryExpectancy = parseInt(Math.round((this.yearsOver()/this.mercury * 100)/100));
@@ -34,27 +34,65 @@ export default class SolarYears {
         return `"You are ${mercuryExpectancy} years over your expected years. Go have more fun!`;
       } else if (mercuryExpectancy < 0) {
       return `"You have ${(Math.abs(mercuryExpectancy))} years left. Go have more fun!"`
+      }
+    } 
+
+    marsAge() {
+     this.age = Math.round(((this.earthYears/this.mars) * 100)/100);
+     return this.age;
     }
-  } 
 
-  marsAge() {
-    this.age = Math.round(((this.earthYears/this.mars) * 100)/100);
-    return this.age;
+    marsExpectancy() {
+      this.expectancy = Math.round(((this.earthExpectancy/this.mars)*100)/100);
+      return this.expectancy;
+    }    
+
+    marsYearsLeftOrOver() {
+      let marsExpectancy = parseInt(Math.round((this.yearsOver()/this.mars * 100)/100));
+      if(marsExpectancy >= 0) {
+        return `"You are ${marsExpectancy} years over your expected years. Go have more fun!`;
+      } else if (marsExpectancy < 0) {
+        return `"You have ${(Math.abs(marsExpectancy))} years left. Go have more fun!"`
+      }
+    } 
+
+    jupiterAge() {
+      this.age = Math.round(((this.earthYears/this.jupiter) * 100)/100);
+      return this.age;
+    }
+
+    jupiterExpectancy() {
+     this.expectancy = Math.round(((this.earthExpectancy/this.jupiter)*100)/100);
+      return this.expectancy;
+    }     
+
+    jupiterYearsLeftOrOver() {
+      let jupiterExpectancy = parseInt(Math.round((this.yearsOver()/this.jupiter * 100)/100));
+      if(jupiterExpectancy >= 0) {
+        return `"You are ${jupiterExpectancy} years over your expected years. Go have more fun!`;
+      } else if (jupiterExpectancy < 0) {
+        return `"You have ${(Math.abs(jupiterExpectancy))} years left. Go have more fun!"`
+      }
+    } 
+
+
+    venusAge() {
+      this.age = Math.round(((this.earthYears/this.venus) * 100)/100);
+      return this.age;
+    }
+
+    venusExpectancy() {
+      this.expectancy = Math.round(((this.earthExpectancy/this.venus)*100)/100);
+      return this.expectancy;
+    }    
+
+    venusYearsLeftOrOver() {
+      let venusExpectancy = parseInt(Math.round((this.yearsOver()/this.venus * 100)/100));
+      if(venusExpectancy >= 0) {
+        return `"You are ${venusExpectancy} years over your expected years. Go have more fun!`;
+      } else if (venusExpectancy < 0) {
+        return `"You have ${(Math.abs(venusExpectancy))} years left. Go have more fun!"`
+      }
+    }
   }
-
-  marsExpectancy() {
-    this.expectancy = Math.round(((this.earthExpectancy/this.mars)*100)/100);
-    return this.expectancy;
-}    
-
-  marsYearsLeftOrOver() {
-    let marsExpectancy = parseInt(Math.round((this.yearsOver()/this.mars * 100)/100));
-    if(marsExpectancy >= 0) {
-      return `"You are ${marsExpectancy} years over your expected years. Go have more fun!`;
-    } else if (marsExpectancy < 0) {
-    return `"You have ${(Math.abs(marsExpectancy))} years left. Go have more fun!"`
-  }
-} 
-
-}
 
